bb47a32285664b896fa03d0fe05f548f0c26cfd8
Samuel Mace <samuelcmace@gmail.com>
2023-01-28 21:21:18 -0600

    Add Log4j Default Configuration File

--- src/main/java/io/github/adventures_in_the_jungle/Main.java
+++ src/main/java/io/github/adventures_in_the_jungle/Main.java
@@ -2,17 +2,28 @@ package io.github.adventures_in_the_jungle;
 
 import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        Logger logger = LogManager.getRootLogger();
+        logger.trace("Configuration File Defined To Be :: " + System.getProperty("log4j.configurationFile"));
+
         System.out.println("Press enter to create the map");
-        scanner.nextLine(); // Wait for the user to press enter
+        scanner.nextLine();
+
         try {
             Map map = new Map();
 
+
         } catch (Exception e) {
 
-            // TODO: handle exception
+
+
         }
     }
 }
