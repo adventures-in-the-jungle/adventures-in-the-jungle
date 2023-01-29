2a958d404d4d7abe2f00cf977c6efb778a78f2e6
Samuel Mace <samuelcmace@gmail.com>
2023-01-28 22:18:09 -0600

    Add Log4J Logfile to .gitignore and Continue Class Development

--- src/main/java/io/github/adventures_in_the_jungle/Item.java
+++ src/main/java/io/github/adventures_in_the_jungle/Item.java
@@ -1,52 +1,68 @@
 package io.github.adventures_in_the_jungle;
 
 public class Item {
+    //Item ID
     private int item_id;
+    //Item name
     private String item_name;
+    //ItemRoomID
     private int room_id;
+    //Item points
     private int item_points;
+    //Item type
     private int item_type;
 
+    //Create Constructor For Item in here
+    public Item() {
 
-
+    }
 
     //Getter for item ID
     public int getItem_id() {
         return item_id;
     }
+
     //Setter for item ID
     public void setItem_id(int item_id) {
         this.item_id = item_id;
     }
+
     //Getter for tem Name
     public String getItem_name() {
         return item_name;
     }
+
     //Setter for item name
     public void setItem_name(String item_name) {
         this.item_name = item_name;
     }
-    //Getter for Itme R
+
+    //Getter for Item Room ID
     public int getRoom_id() {
         return room_id;
     }
 
+    //Setter for Item Room ID
     public void setRoom_id(int room_id) {
         this.room_id = room_id;
     }
 
+    //Getter for Item Points
     public int getItem_points() {
         return item_points;
     }
 
+    //Setter for Item Points
     public void setItem_points(int item_points) {
         this.item_points = item_points;
     }
 
+    //Getter for Item Type
     public int getItem_type() {
         return item_type;
     }
 
+    //Setter for Item Type
     public void setItem_type(int item_type) {
         this.item_type = item_type;
     }
--- src/main/java/io/github/adventures_in_the_jungle/Main.java
+++ src/main/java/io/github/adventures_in_the_jungle/Main.java
@@ -1,57 +1,43 @@
 package io.github.adventures_in_the_jungle;
 
-import java.util.HashMap;
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
-    public HashMap<String, Room> rooms;
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         Logger logger = LogManager.getRootLogger();
-
-        logger.log(Level.DEBUG, "Debug!");
-        logger.log(Level.ERROR, "Error!");
-        logger.log(Level.ERROR, "Error!");
-        logger.log(Level.ERROR, "Error!");
-        logger.log(Level.INFO, "Info!");
-
-        int SIZE = 5;
+        logger.info("Game Started!");
 
         System.out.println("Press enter to create the map");
         scanner.nextLine();
 
-        /*
-
-        
         Random rand = new Random();
+        int SIZE = 5;
         int randomNum = rand.nextInt(SIZE);
 
+        HashMap<String, Room> rooms;
+
         rooms = new HashMap<>();
-          for (int i = 0; i < SIZE; i++) {
+        for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-              Room room = new Room(i, j); }   }
-
-            try{
-
-            Room endRoom = new Room(3,3);
-            Room randomRoom = new Room(randomNum,randomNum);
-
-        }catch (Exception e) {
-                System.out.println("Not the End Room");
+                Room room = new Room(i, j);
+            }
         }
-        */
+
+        try {
+
+            Room endRoom = new Room(randomNum, randomNum);
+
+        } catch (Exception e) {
+            logger.fatal("There was a fatal exception thrown in the rooms! Abort!");
+        }
+
     }
 }
-
-
-//
-//
-//
-//
